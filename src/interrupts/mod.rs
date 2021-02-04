dd32fdee5bd98124756eff53ed3870fc884615eb
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-02-04 13:26:45 +0100

    filled interruption list

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -30,15 +30,23 @@ lazy_static! {
     static ref IDT : InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.divide_error.set_handler_fn(divide_error_handler);
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        idt.divide_error.set_handler_fn(divide_error_handler);
-        idt.overflow.set_handler_fn(overflow_handler);
         idt.debug.set_handler_fn(debug_handler);
         idt.non_maskable_interrupt.set_handler_fn(non_maskable_interrupt_handler);
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.overflow.set_handler_fn(overflow_handler);
         idt.bound_range_exceeded.set_handler_fn(bound_range_exceeded_handler);
         idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
-        //idt.double_fault.set_handler_fn(double_fault_handler);
+        idt.device_not_available.set_handler_fn(device_not_available_handler);
+        idt.invalid_tss.set_handler_fn(invalid_tss_handler);
+        idt.segment_not_present.set_handler_fn(segment_not_present_handler);
+        idt.stack_segment_fault.set_handler_fn(stack_segment_fault_handler);
+        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.x87_floating_point.set_handler_fn(x87_floating_point_handler);
+        idt.alignment_check.set_handler_fn(alignment_check_handler);
+        idt.simd_floating_point.set_handler_fn(simd_floating_point_handler);
+        idt.virtualization.set_handler_fn(virtualization_handler);
+        idt.security_exception.set_handler_fn(security_exception_handler);
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
         unsafe {
@@ -120,6 +128,25 @@ extern "x86-interrupt" fn general_protection_fault_handler(_stack_frame : &mut I
     panic!("GENERAL PROTECTION FAULT");
 }
 
+extern "x86-interrupt" fn x87_floating_point_handler(_stack_frame : &mut InterruptStackFrame) {
+    panic!("x87 FLOATING POINT");
+}
+
+extern "x86-interrupt" fn alignment_check_handler(_stack_frame : &mut InterruptStackFrame, _error_code : u64) {
+    panic!("ALIGNMENT CHECK");
+}
+
+extern "x86-interrupt" fn simd_floating_point_handler(_stack_frame : &mut InterruptStackFrame) {
+    panic!("SIMD FLOATING POINT");
+}
+
+extern "x86-interrupt" fn virtualization_handler(_stack_frame : &mut InterruptStackFrame) {
+    panic!("VIRTUALIZATION");
+}
+
+extern "x86-interrupt" fn security_exception_handler(_stack_frame : &mut InterruptStackFrame, _error_code : u64) {
+    panic!("SECURITY EXCEPTION");
+}
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame : &mut InterruptStackFrame) {
     //print!(".");
