1c3e2400b2b060e58a483f23542405fb37d9e3e7
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-02-04 10:28:09 +0100

    Allocator

--- src/allocator/mod.rs
+++ src/allocator/mod.rs
@@ -1,5 +1,10 @@
 use alloc::alloc::{GlobalAlloc, Layout};
+use x86_64::{addr::VirtAddr, structures::paging::{FrameAllocator, Page, PageTableFlags, Size4KiB, Mapper, mapper::MapToError}};
 use core::ptr::null_mut;
+use linked_list_allocator::LockedHeap; //Will be removed in favor of a custom allocator in the future
+
+pub const HEAP_START : usize = 0x4444_4444_0000;
+pub const HEAP_SIZE : usize = 100 * 1024;
 
 #[alloc_error_handler]
 fn alloc_error_handler(layout : alloc::alloc::Layout) -> ! {
@@ -19,4 +24,28 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 #[global_allocator]
-static ALLOCATOR: Dummy = Dummy;
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+pub fn init(mapper: &mut impl Mapper<Size4KiB>, frame_allocator : &mut impl FrameAllocator<Size4KiB>)
+    -> Result<(), MapToError<Size4KiB>> {
+        let page_range = {
+            let heap_start = VirtAddr::new(HEAP_START as u64);
+            let heap_end = heap_start + HEAP_SIZE - 1u64;
+            let heap_start_page = Page::containing_address(heap_start);
+            let heap_end_page = Page::containing_address(heap_end);
+            Page::range_inclusive(heap_start_page, heap_end_page)
+        };
+        for page in page_range {
+            let frame = frame_allocator.allocate_frame().ok_or(MapToError::FrameAllocationFailed)?;
+            let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+            unsafe {
+                mapper.map_to(page, frame, flags, frame_allocator)?.flush();
+            };
+        }
+
+        unsafe {
+            ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
+        }
+
+        Ok(())
+    }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use x86_64::addr::VirtAddr;
+use x86_64::addr::{VirtAddr, VirtAddrNotValid};
 use x86_64::structures::paging::Translate;
 mod vga;
 mod interrupts;
@@ -39,9 +39,12 @@ entry_point!(kernel_main);
 /// This is the starting function. Its name must not be changeed by the compiler, hence the `#![no_mangle]`
 fn kernel_main(_boot_info : &'static BootInfo) -> ! {
     init();
+    let phys_mem_offset = VirtAddr::new(_boot_info.physical_memory_offset);
+    let mut mapper = unsafe {memory::init(phys_mem_offset)};
     let mut frame_allocator = unsafe {
         memory::BootInfoAllocator::init(&_boot_info.memory_map)
     };
+    allocator::init(&mut mapper, &mut frame_allocator).expect("Heap init failed :((");
     for i in 0..10 {
         println!("{}", i);
     }
@@ -55,7 +58,7 @@ fn kernel_main(_boot_info : &'static BootInfo) -> ! {
     }
 
     let ptr = 0xdeadbeaf as *mut u32;
-    unsafe { *ptr = 42; }
+    //unsafe { *ptr = 42; }
 
     let x = Box::new([0, 1]);
 
