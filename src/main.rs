02c3dd0e00fc29b0f208c9904464fcdedf7514f1
Paul Fournier <pfournier01@clipper.ens.fr>
2021-02-13 15:54:49 +0100

    add: documented code

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,10 @@ mod keyboard;
 mod memory;
 mod task;
 
+
+/// # The core of the FerrOS operating system.
+/// It's here that we perform the Frankenstein magic of assembling all the parts together.
+
 use crate::task::{executor::Executor, Task};
 
 extern crate alloc;
@@ -34,6 +38,7 @@ extern crate alloc;
 use alloc::boxed::Box;
 use alloc::string::String;
 
+/// # Panic handling
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -41,23 +46,38 @@ fn panic(_info: &PanicInfo) -> ! {
     halt_loop();
 }
 
+/// Halts forever
 pub fn halt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
 }
 
+/// Halts for some time
 pub fn long_halt(i: usize) {
     for _ in 0..i {
         x86_64::instructions::hlt();
     }
 }
 
-pub fn init() {
+/// # Initialization
+/// Initializes the configurations
+pub fn init(_boot_info : &'static BootInfo) {
     interrupts::init();
     gdt::init();
+    
+    // Memory allocation Initialization
+    let phys_mem_offset = VirtAddr::new(_boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { memory::BootInfoAllocator::init(&_boot_info.memory_map) };
+    allocator::init(&mut mapper, &mut frame_allocator).expect("Heap init failed :((");
+
+    // I/O Initialization
+    keyboard::init();
+    vga::init();
 }
 
+// test taks, to move out of here
 async fn task_1() {
     loop {
         print!("X");
@@ -65,6 +85,7 @@ async fn task_1() {
     }
 }
 
+// test task, to move out of here
 async fn task_2() {
     loop {
         print!("0");
@@ -73,21 +94,20 @@ async fn task_2() {
 }
 
 entry_point!(kernel_main);
-/// This is the starting function.
-fn kernel_main(_boot_info: &'static BootInfo) -> ! {
-    init();
-    let phys_mem_offset = VirtAddr::new(_boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { memory::BootInfoAllocator::init(&_boot_info.memory_map) };
-    allocator::init(&mut mapper, &mut frame_allocator).expect("Heap init failed :((");
+// We use it to check a)t compile time that we are doing everything correctly with the arguments of `kernel_main`
 
-    keyboard::init();
-    vga::init();
+/// # Entry point
+/// This is the starting function, it's here that the bootloader sends us to when starting the system.
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+    init(_boot_info);
+    // Why is this not in the init function ?
+
 
     // This enables the tests
     #[cfg(test)]
     test_main();
 
+    // Yet again, some ugly tests in main
     programs::shell::main_shell();
     println!();
     for i in 0..5 {
