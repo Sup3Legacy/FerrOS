ad59cb4d1bc69196d3f9c51e5d0fb539a8a0b347
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-02-21 13:48:19 +0100

    gestion des process dans un tableau

--- src/scheduler/process.rs
+++ src/scheduler/process.rs
@@ -2,6 +2,7 @@ use super::PROCESS_MAX_NUMBER;
 use alloc::vec::Vec;
 use core::sync::atomic::{AtomicU64, Ordering};
 use spin::Mutex;
+use lazy_static::lazy_static;
 
 /// Main structure of a process.
 /// It contains all informations about a process and its operating frame.
@@ -15,6 +16,7 @@ use spin::Mutex;
 /// * `state` - state of the process
 /// * `children` - vec containing the processes it spawned.
 /// * `value` - return value
+/// * `owner` - owner ID of the process (can be root or user) usefull for syscalls
 #[derive(Debug)]
 pub struct Process {
     id: ID,
@@ -22,27 +24,48 @@ pub struct Process {
     priority: Priority,
     quantum: u64,
     cr3: usize,
+    rsp: usize,
     state: State,
-    children: Vec<Mutex<Process>>,
+  //  children: Vec<Mutex<Process>>, // Maybe better to just store ID's
+    children: Vec<ID>,
     value: usize,
+    owner: u64,
 }
 
 impl Process {
-    pub fn new(parent: ID, priority: Priority) -> Self {
+    pub fn create_new(parent: ID, priority: Priority, owner: u64) -> Self {
         Self {
             id: ID::new(),
             pid: parent,
             priority,
             quantum: 0 as u64,
             cr3: 42, // /!\
+            rsp: 0, // /!\
             state: State::Runnable,
             children: Vec::new(),
             value: 0,
+            owner,
         }
     }
-    pub fn spawn(&mut self, priority: Priority) -> &Mutex<Self> {
-        let child = Process::new(self.id, priority);
-        self.children.push(Mutex::new(child));
+
+    pub fn missing() -> Self {
+        Self {
+            id: ID(0),
+            pid: ID(0),
+            priority: Priority(0),
+            quantum: 0 as u64,
+            cr3: 0,
+            rsp: 0,
+            state: State::SlotAvailable,
+            children: Vec::new(),
+            value: 0,
+            owner: 0,
+        }
+    }
+
+    pub fn spawn(&mut self, priority: Priority) -> &ID {// -> &Mutex<Self> {
+        let child = Process::create_new(self.id, priority, self.owner);
+        self.children.push(child.id);//Mutex::new(child));
         &(self.children[self.children.len() - 1])
     }
 }
@@ -60,6 +83,7 @@ pub enum State {
     SleepInterruptible,
     SleepUninterruptible,
     Stopped,
+    SlotAvailable,
 }
 
 /// A process's ID.
@@ -72,12 +96,34 @@ impl ID {
     /// Returns a fresh ID. It uses an atomic operation to make sure no two processes can have the same id.
     ///
     /// For now, a process's id isn't freed when it exits.
-    pub fn new() -> Self {
+    pub fn new_old() -> Self {
         static NEXT_ID: AtomicU64 = AtomicU64::new(0);
-        let new = NEXT_ID.fetch_add(1, Ordering::Relaxed);
+        let new = NEXT_ID.fetch_add(1, Ordering::Relaxed); // Maybe better to reallow previous numbers
         if new >= PROCESS_MAX_NUMBER {
             panic!("Reached maximum number of processes!");
         }
         Self(new)
     }
+
+    pub fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        for i in 0..PROCESS_MAX_NUMBER {
+            let new = NEXT_ID.fetch_add(1, Ordering::Relaxed);
+            match ID_TABLE[(new % PROCESS_MAX_NUMBER) as usize].state {
+                State::SlotAvailable => return ID(new),
+                _ => ()
+            }
+        }
+        panic!("no slot available")
+    }
+}
+
+
+lazy_static! {
+    static ref ID_TABLE: [Process; PROCESS_MAX_NUMBER as usize] = [
+        Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(),
+        Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(),
+        Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(),
+        Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing(), Process::missing()
+        ];
 }
