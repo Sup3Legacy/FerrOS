741bfc1d8eaaf533a3135dfc94cfd5fe66094a34
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-03-06 22:21:02 +0100

    a few small modif

--- src/filesystem/disk_operations.rs
+++ src/filesystem/disk_operations.rs
@@ -22,7 +22,7 @@ pub fn test_read() {
 /// Initialise the disk by reading it's informations (should improve it by giving an output)
 pub fn init() {
     unsafe {
-        disable();
+       // disable();
         let mut data_register = Port::<u16>::new(0x170); // used to read write PIO data
         let mut sectorcount_register = Port::new(0x172);
         let mut lba_low = Port::new(0x173);
@@ -38,6 +38,7 @@ pub fn init() {
         //        println!("command send");
 
         command_register.write(0xEC_u8);
+
         let mut i = command_register.read();
         let mut compte = 1;
         while (i & 0x8) == 0 {
@@ -51,7 +52,7 @@ pub fn init() {
         for i in 0..256 {
             data_table[i] = data_register.read();
         }
-        /*
+        
         println!("uint16_t 0 : {}", data_table[0]);
         println!("uint16_t 83 : {} {}", data_table[83], data_table[83] & 1024);
         println!("uint16_t 88 : {}", data_table[88]);
@@ -60,8 +61,9 @@ pub fn init() {
         println!("uint32_t 100-103 : {}", ((data_table[100] as u64) << 0) |
                         ((data_table[101] as u64) << 16) |
                         ((data_table[102] as u64) << 32) | ((data_table[103] as u64) << 48));
-                        */
-        enable(); // /!\ Should not to this if it was disabled before !
+                   
+        wait_BSY(DISK_PORT);
+      //  enable(); // /!\ Should not to this if it was disabled before !
     }
 }
 
@@ -72,27 +74,9 @@ pub fn read_sector(lba: u32) -> [u16; 256] {
         read((&mut a) as *mut [u16; 256], lba, DISK_PORT);
     }
     unsafe {
-        //flush_cache(DISK_PORT);
+        flush_cache(DISK_PORT);
     }
     a
-}
-
-unsafe fn flush_cache(port: u16) {
-    disable();
-    let mut command_register = Port::new(port + 7);
-
-    command_register.write(0xE7_u8); // give the READ SECTOR command
-    let mut i = command_register.read();
-    let mut compte = 1;
-    while (i & 0x80) != 0 {
-        //println!("{} en {}", i, compte); // to warn in case of infinite loops
-        i = command_register.read();
-        compte += 1;
-        if compte % 1000000 == 0 {
-            println!("not finished 1-1 : {} en {}", i, compte); // to warn in case of infinite loops
-        }
-    }
-    enable();
 }
 
 /// Read function that reads in any disk if the right port is given.
@@ -103,6 +87,7 @@ unsafe fn read(table: *mut [u16; 256], lba: u32, port: u16) {
 
     //println!("Reading from sector {}", lba);
     let lba = lba as u64;
+
     let mut data_register = Port::<u16>::new(port + 0);
     let mut sectorcount_register = Port::new(port + 2);
     let mut lba_low = Port::new(port + 3);
@@ -110,6 +95,7 @@ unsafe fn read(table: *mut [u16; 256], lba: u32, port: u16) {
     let mut lba_high = Port::new(port + 5);
     let mut drive_head_register = Port::new(port + 6);
     let mut command_register = Port::new(port + 7);
+
     drive_head_register.write(0x40_u8); // outb(0x1F6, 0xE0 | (slavebit << 4) | ((LBA >> 24) & 0x0F))
     sectorcount_register.write(0_u8); // says that we want to read only one register
     lba_low.write((lba >> 24) as u8);
@@ -121,10 +107,7 @@ unsafe fn read(table: *mut [u16; 256], lba: u32, port: u16) {
     lba_high.write((lba >> 16) as u8);
     command_register.write(0x24_u8); // give the READ SECTOR command
 
-    // waits for the disk to be ready for transfer
-    let mut i = command_register.read();
-    let mut compte = 1;
-    
+    // waits for the disk to be ready for transfer    
     wait_BSY(port);
     wait_DRQ(port);
 
@@ -132,9 +115,6 @@ unsafe fn read(table: *mut [u16; 256], lba: u32, port: u16) {
         let t = data_register.read(); // reads all the data one by one. The loop is mandatory to give the drive the time to give the data
         (*table)[i] = t;
     }
-
-    let mut i = command_register.read();
-    let mut compte = 1;
     
     wait_BSY(port);
 
@@ -158,6 +138,7 @@ unsafe fn write(table: &[u16; 256], lba: u32, port: u16) {
     wait_BSY(port);
 
     let lba = lba as u64;
+
     let mut data_register = Port::<u16>::new(port + 0);
     let mut sectorcount_register = Port::new(port + 2);
     let mut lba_low = Port::new(port + 3);
@@ -165,6 +146,7 @@ unsafe fn write(table: &[u16; 256], lba: u32, port: u16) {
     let mut lba_high = Port::new(port + 5);
     let mut drive_head_register = Port::new(port + 6);
     let mut command_register = Port::new(port + 7);
+
     drive_head_register.write(0x40_u8); // outb(0x1F6, 0xE0 | (slavebit << 4) | ((LBA >> 24) & 0x0F))
     sectorcount_register.write(0_u8); // says that we want to write only one register
     lba_low.write((lba >> 24) as u8);
@@ -176,8 +158,6 @@ unsafe fn write(table: &[u16; 256], lba: u32, port: u16) {
     lba_high.write((lba >> 16) as u8);
     command_register.write(0x34_u8); // give the READ SECTOR command
 
-    let mut i = command_register.read();
-    let mut compte = 1;
     
     wait_BSY(port);
     wait_DRQ(port);
@@ -188,10 +168,7 @@ unsafe fn write(table: &[u16; 256], lba: u32, port: u16) {
         delay.write(0_u8);
     }
 
-    command_register.write(0xE7);
-
-    let mut i = command_register.read();
-    let mut compte = 1;
+    command_register.write(0xE7); 
     
     wait_BSY(port);
 
