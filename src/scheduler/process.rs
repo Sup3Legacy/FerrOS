5d906c8ac3d82dbba77d3106cad23532c55af698
Paul Fournier <pfournier01@clipper.ens.fr>
2021-03-30 15:33:57 +0200

    light cleanup

--- src/scheduler/process.rs
+++ src/scheduler/process.rs
@@ -311,27 +311,6 @@ pub unsafe fn disassemble_and_launch(
     loop {}
 }
 
-/*
-    mov rsp, rdi
-    pop rbx
-    pop rcx
-    pop rbp
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-    pop r9
-    pop r8
-    pop r10
-    pop rdx
-    pop rsi
-    pop rdi
-    pop rax
-    sti
-    iretq
-*/
-
 /// Main structure of a process.
 /// It contains all informations about a process and its operating frame.
 /// It is based on the x86 structure of the TSS.
@@ -342,10 +321,9 @@ pub unsafe fn disassemble_and_launch(
 /// * `priority` - the priority, used by the scheduler (not used for now)
 /// * `quantum` - the number of consecutive quanta the process has already been running for
 /// * `cr3` - pointer to its 1st order VM table. TO DO : replace it with a PhysFrame or PhysAddr
+/// * `cr3f` - cr3 flags ???
 /// * `rip` - current value of the instruction pointer
 /// * `state` - state of the process (e.g. Zombie, Runnable...)
-/// * `children` - vec containing the processes it spawned.
-/// * `value` - return value
 /// * `owner` - owner ID of the process (can be root or user) usefull for syscalls
 #[derive(Copy,Clone,Debug)]
 #[repr(C)]
@@ -357,11 +335,7 @@ pub struct Process {
     pub cr3: PhysAddr,
     pub cr3f: Cr3Flags,
     pub rsp: u64, // every registers are saved on the stack
-    //pub stack_frame: InterruptStackFrameValue,
-    //pub registers: Registers,
     state: State,
-    //children: Vec<ID>,
-    value: usize, // Return value => only meaningful when the process has finished, perhaps put it in the State?
     owner: u64,
 }
 
@@ -379,11 +353,7 @@ impl Process {
             cr3: PhysAddr::zero(),
             cr3f: Cr3Flags::empty(),
             rsp: 0,
-            //stack_frame: InterruptStackFrameValue::empty(),
-            //registers: Registers::new(),
             state: State::Runnable,
-            // children: Vec::new(),
-            value: 0,
             owner,
         }
     }
@@ -397,11 +367,7 @@ impl Process {
             cr3: PhysAddr::zero(),
             cr3f: Cr3Flags::empty(),
             rsp: 0,
-            //stack_frame: InterruptStackFrameValue::empty(),
-            //registers: Registers::new(),
             state: State::SlotAvailable,
-            // children: Vec::new(),
-            value: 0,
             owner: 0,
         }
     }
@@ -429,83 +395,6 @@ impl Process {
         launch_asm(f, 0);
     }
 
-/*
-    #[naked]
-    unsafe fn save_state() {
-        asm!(
-            "push rax", // save rax
-            "mov rax, [rsp+24]", // rax <- rip
-            "mov [{0}], rax", // store rip
-            "mov rax, cr3", // rax <- cr3
-            "mov [{0}+8], rax", // store cr3
-            "pop rax",
-
-            "sub rsp, 8", // remove rip from the stack : we want to add it manually after
-
-            // continue to save the other registers
-            "push rbx",
-            "push rcx",
-            "push rdx",
-            "push rbp",
-            "push rsp",
-            "push rsi",
-            "push rdi",
-            "push r8",
-            "push r9",
-            "push r10",
-            "push r11",
-            "push r12",
-            "push r13",
-            "push r14",
-            "push r15",
-
-            // save the flags
-            "pushfq",
-            sym CURRENT_PROCESS,
-            options(noreturn)
-        );
-    }
-
-    #[naked]
-    unsafe fn load_state() {
-        asm!(
-            // restore flags
-            "popfq",
-
-            //restore registers
-            "pop r15",
-            "pop r14",
-            "pop r13",
-            "pop r12",
-            "pop r11",
-            "pop r10",
-            "pop r9",
-            "pop r8",
-            "pop rdi",
-            "pop rsi",
-            "pop rsp",
-            "pop rbp",
-            "pop rdx",
-            "pop rcx",
-            "pop rbx",
-
-            //restore cr3 (paging)
-            "mov rax, [{0} + 8]", // cr3
-            "mov cr3, rax",
-
-            // restore rip
-            "mov rax, [{0}]", // rip
-            "mov [rsp+8], rax",
-
-            // restore rax
-            "pop rax",
-
-            "ret",
-            sym CURRENT_PROCESS,
-            options(noreturn)
-        );
-    }
-*/
 }
 
 // Keeps track of the children of the processes, in order to keep the Process struct on the stack
@@ -520,7 +409,7 @@ pub struct Priority(usize);
 pub enum State {
     Runnable,
     Running,
-    Zombie,
+    Zombie(usize),
     SleepInterruptible,
     SleepUninterruptible,
     Stopped,
@@ -529,7 +418,7 @@ pub enum State {
 
 /// A process's ID.
 ///
-/// It's uniqueness throughout the system is ensured by the atomic `fetch_add` operation.
+/// Its uniqueness throughout the system is ensured by the atomic `fetch_add` operation.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(transparent)]
 pub struct ID(u64);
@@ -538,15 +427,6 @@ impl ID {
     /// Returns a fresh ID. It uses an atomic operation to make sure no two processes can have the same id.
     ///
     /// For now, a process's id isn't freed when it exits.
-    pub fn new_old() -> Self {
-        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
-        let new = NEXT_ID.fetch_add(1, Ordering::Relaxed); // Maybe better to reallow previous numbers
-        if new >= PROCESS_MAX_NUMBER {
-            panic!("Reached maximum number of processes!");
-        }
-        Self(new)
-    }
-
     pub fn new() -> Self {
         static NEXT_ID: AtomicU64 = AtomicU64::new(0);
         for _i in 0..PROCESS_MAX_NUMBER {
