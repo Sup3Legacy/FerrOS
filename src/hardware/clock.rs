4960d01faaf7b86d1fbc2053868b0515f6486a04
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-03-31 14:30:12 +0200

    RTC working!

--- src/hardware/clock.rs
+++ src/hardware/clock.rs
@@ -3,21 +3,26 @@ use x86_64::instructions::port::Port;
 use crate::println;
 
 const CMOS_ADDRESS: u16 = 0x70;
-static mut CMOS_ADDRESS_PORT: Port<u16> = Port::new(CMOS_ADDRESS);
+static mut CMOS_ADDRESS_PORT: Port<u8> = Port::new(CMOS_ADDRESS);
 const CMOS_DATA: u16 = 0x71;
 static mut CMOS_DATA_PORT: Port<u8> = Port::new(CMOS_DATA);
 
 /// # Safety
 /// TODO
-unsafe fn get_rtc(reg: u16) -> u8 {
+unsafe fn get_rtc(reg: u8) -> u8 {
     CMOS_ADDRESS_PORT.write(reg);
     CMOS_DATA_PORT.read()
 }
 
 /// # Safety
 /// TODO
-unsafe fn get_update() -> u8 {
-    get_rtc(0x0A) & 0x80
+unsafe fn wait_update() {
+    //while get_rtc(0x0A) & 0x80 != 0x80 {}
+    while get_rtc(0x0A) & 0x80 == 0x80 {}
+}
+
+fn cvt_bcd(value: usize) -> usize {
+    ((value & 0xF) + ((value / 16) * 10))
 }
 
 #[derive(Debug)]
@@ -49,68 +54,59 @@ impl WeekDay {
 #[derive(Debug)]
 pub struct Time {
     century: usize,
-    last_second: usize,
-    last_minute: usize,
-    last_hour: usize,
-    last_day: usize,
-    last_month: usize,
-    last_year: usize,
-    last_century: usize,
+    second: usize,
+    minute: usize,
+    hour: usize,
+    day: usize,
+    month: usize,
+    year: usize,
 }
 
 impl Time {
     /// # Safety
     /// TODO
     pub unsafe fn get() -> Self {
-        while get_update() != 0 {}
-        let mut second = get_rtc(0x00);
-        let mut minute = get_rtc(0x02);
-        let mut hour = get_rtc(0x04);
-        let mut day = get_rtc(0x07);
-        let mut month = get_rtc(0x08);
-        let mut year = get_rtc(0x09);
-        // + 2 to enter the while
-        let mut last_second;
-        let mut last_minute;
-        let mut last_hour;
-        let mut last_day;
-        let mut last_month;
-        let mut last_year;
+        asm!("cli");
+        let mut second;
+        let mut minute;
+        let mut hour;
+        let mut day;
+        let mut month;
+        let mut year;
+        let mut century;
+        let register_b;
+        wait_update();
+        second = get_rtc(0x00) as usize;
+        minute = get_rtc(0x02) as usize;
+        hour = get_rtc(0x04) as usize;
+        day = get_rtc(0x07) as usize;
+        month = get_rtc(0x08) as usize;
+        year = get_rtc(0x09) as usize;
+        century = 20_usize;
+        register_b = get_rtc(0x0b);
 
-        loop {
-            println!("Clock update.");
-            last_second = second;
-            last_minute = minute;
-            last_hour = hour;
-            last_day = day;
-            last_month = month;
-            last_year = year;
-            while get_update() != 0 {}
-            second = get_rtc(0x00);
-            minute = get_rtc(0x02);
-            hour = get_rtc(0x04);
-            day = get_rtc(0x07);
-            month = get_rtc(0x08);
-            year = get_rtc(0x09);
-            if !(last_second != second
-                || last_minute != minute
-                || last_hour != hour
-                || last_day != day
-                || last_month != month
-                || last_year != year)
-            {
-                break;
-            }
+        if register_b & 4 != 4 {
+            second = cvt_bcd(second);
+            minute = cvt_bcd(minute);
+            hour = cvt_bcd(hour & 0x7F) | (hour & 0x80);
+            day = cvt_bcd(day);
+            month = cvt_bcd(month);
+            year = cvt_bcd(year);
         }
+
+        if register_b & 2 != 2 || hour & 0x80 == 0x80 {
+            hour = ((hour & 0x7F) + 12) % 24;
+        }
+
+        asm!("sti");
         Self {
-            century: 0,
-            last_second: ((second & 0x0F) + ((second >> 4) * 10)) as usize,
-            last_minute: ((minute & 0x0F) + ((minute >> 4) * 10)) as usize,
-            last_hour: ((hour & 0x0F) + ((hour >> 4) * 10)) as usize,
-            last_day: ((day & 0x0F) + ((day >> 4) * 10)) as usize,
-            last_month: ((month & 0x0F) + ((month >> 4) * 10)) as usize,
-            last_year: ((year & 0x0F) + ((year >> 4) * 10)) as usize + 2000,
-            last_century: 0_usize,
+            century,
+            second,
+            minute,
+            hour,
+            day,
+            month,
+            year: year + 100 * century,
         }
     }
 }
