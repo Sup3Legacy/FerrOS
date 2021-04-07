7831b886eb6c983b4ac22e40ca882ea5892c8df9
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-04-07 14:36:46 +0200

    updated .gitignore + filesystem-scraping working

--- createDisk.py
+++ createDisk.py
@@ -1,6 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
 
 from diskStructures import *
+import os
 
 # Path to filesystem directory
 directory = "./filesystem/"
@@ -13,3 +14,21 @@ l2 = l2 + [0]*(512-len(l2))
 print('a'*512*n ,end="", file = disque)
 disque.close()
 
+def construct_filesystem_tree(path):
+    files = []
+    name = ""
+    for child in os.listdir(path):
+        child_path = os.path.join(path, child)
+        if os.path.isfile(child_path):
+            name = child
+            data = open(child_path, "rb")
+            files.append(File(data, name))
+        else:
+            # if the child is a directory
+            subtree = construct_filesystem_tree(child_path)
+            subtree.header.name = child
+            files.append(subtree)
+    return Dir(files, name)
+
+
+construct_filesystem_tree(directory)
